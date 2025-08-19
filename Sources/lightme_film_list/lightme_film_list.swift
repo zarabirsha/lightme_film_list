eb55fea5a02989433905ae8ce5eec8ebe914c554
zarabirsha <zarabirsha@gmail.com>
2025-08-19 13:04:37 +0200

    good

--- Sources/lightme_film_list/lightme_film_list.swift
+++ Sources/lightme_film_list/lightme_film_list.swift
@@ -1,3 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-public struct LightmeFilmList {}
+import Foundation
+public struct LightmeFilmList {
+    public static let json: URL = {
+            return Bundle.module.url(forResource: "films", withExtension: "json")!
+        }()
+    
+}
